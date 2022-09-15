581436f60af176bc46f7f1c4aff62cecbbf4fada
Igson <igson660@gmail.com>
2022-09-15 16:17:20 -0500

    :tada: feat: menu mobile

---
+++ index.js
@@ -0,0 +1,8 @@
+const $ = document.querySelector.bind(document);
+
+const toggleMenu = () => {
+  $("#menu").classList.toggle("active");
+};
+
+$("#menu-hamburger__open").addEventListener("click", toggleMenu);
+$("#menu-hamburger___close").addEventListener("click", toggleMenu);
